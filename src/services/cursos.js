9a6ee864e29794a5fce81239fa5256b5eee288b3
Alanluz34 <alandeluz@hotmail.com>
2021-04-28 20:11:02 -0300

    first commit

---
+++ src/services/cursos.js
@@ -0,0 +1,23 @@
+class CursoService {
+    constructor(CursoModel) {
+        this.curso = CursoModel
+    }
+
+    async get() {
+        const cursos = await this.curso.findAll()
+        return cursos
+    }
+
+    async adicionar(cursoDTO) {
+        // verifique se já existe curso com o mesmo nome
+        try{
+        await this.curso.create(cursoDTO)
+        }catch(erro){
+            console.erro(erro.message)
+            throw erro
+        }
+    }
+}
+
+
+module.exports = CursoService
